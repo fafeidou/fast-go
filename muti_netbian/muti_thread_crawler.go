d7ae3b8728d32be7059befc2b9b57def2fa5cedd
qfx <qinfuxiang>
2020-01-10 16:17:24 +0800

    update

--- muti_netbian/muti_thread_crawler.go
+++ muti_netbian/muti_thread_crawler.go
@@ -10,8 +10,11 @@ import (
 	"path"
 	"regexp"
 	"strconv"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
 
 // Requirements: multitask crawl all the photos on the first page of each post
 
@@ -33,12 +36,33 @@ func SaveImage(imageUrl string) {
 	filePath := "/Users/batman/Downloads/image/" + path.Base(imageUrl)
 	f, _ := os.Create(filePath)
 	resp, _ := http.Get(imageUrl)
+	if resp == nil {
+		return
+	}
 	defer f.Close()
 	defer resp.Body.Close()
 	reader := bufio.NewReaderSize(resp.Body, 32*1024)
 	writer := bufio.NewWriter(f)
-	_, _ = io.Copy(writer, reader)
-	fmt.Println("图片保存完毕")
+	_, err := io.Copy(writer, reader)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("图片保存完毕" + imageUrl)
+}
+func HandleDetailContent(content string) {
+	reg2 := regexp.MustCompile("/tupian.*html")
+	seedUrl := "http://pic.netbian.com"
+	detailUrl := seedUrl + reg2.FindString(content)
+	Content := HandleUrl(detailUrl)
+	imgReg := regexp.MustCompile("<img src=\".*?\" data-pic=")
+	imgSlice := imgReg.FindAllString(Content, -1)
+	imgReg2 := regexp.MustCompile("/uploads.*jpg")
+	for _, j := range imgSlice {
+		imgUrl := seedUrl + imgReg2.FindString(j)
+		go SaveImage(imgUrl)
+		//wg.Add(1)
+	}
+	//wg.Done()
 }
 
 // Process each list page and extract the details page url
@@ -47,20 +71,11 @@ func HandleListContent(listContent string) {
 	//<a href="/tupian/21953.html" target="_blank">
 	reg := regexp.MustCompile("<a href=\".*?\" target=")
 	resultSlice := reg.FindAllString(listContent, -1)
-	reg2 := regexp.MustCompile("/tupian.*html")
-
-	seedUrl := "http://pic.netbian.com"
 	for _, i := range resultSlice {
-		detailUrl := seedUrl + reg2.FindString(i)
-		Content := HandleUrl(detailUrl)
-		imgReg := regexp.MustCompile("<img src=\".*?\" data-pic=")
-		imgSlice := imgReg.FindAllString(Content, -1)
-		imgReg2 := regexp.MustCompile("/uploads.*jpg")
-		for _, j := range imgSlice {
-			imgUrl := seedUrl + imgReg2.FindString(j)
-			SaveImage(imgUrl)
-		}
+		go HandleDetailContent(i)
+		//wg.Add(1)
 	}
+	//wg.Done()
 }
 
 // Build the url for each list page
@@ -73,13 +88,17 @@ func runListUrl() {
 		} else {
 			Content = HandleUrl(startUrl + "_" + strconv.Itoa(i) + ".html")
 		}
-		HandleListContent(Content)
+		go HandleListContent(Content)
+		//wg.Add(1)
 	}
+	//wg.Done()
 }
 
 func main() {
+	ch := make(chan int)
 	t := time.Now()
 	runListUrl()
 	elapsed := time.Since(t)
+	ch <- 1
 	fmt.Println("app elapsed:", elapsed)
 }
